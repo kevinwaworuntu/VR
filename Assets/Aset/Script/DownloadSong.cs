b57a4f0b3091a5ea21db2b4e0db586717be9f077
kevinaajw <k.waworuntu1@gmail.com>
2021-02-18 12:55:14 +0700

    Get Audio from Server
    Get audio from server + store song data in JSON

---
+++ Assets/Aset/Script/DownloadSong.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Networking;
+using UnityEngine;
+
+public class DownloadSong : MonoBehaviour
+{
+    private string url;
+   // public string songTitle; //ganti nama
+    public AudioSource audioSource;
+    public AudioClip audioClip;
+    public SongDataReader songDataReader;
+
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator DownloadAudioClip()
+    {
+        using (UnityWebRequest webRequest = UnityWebRequestMultimedia.GetAudioClip(url, AudioType.MPEG))
+        {
+            //Request and wait for the desired page.
+            yield return webRequest.SendWebRequest();
+
+            if (webRequest.isNetworkError)
+            {
+                Debug.Log("Error: " + webRequest.error);
+            }
+            else
+            {
+                audioClip = DownloadHandlerAudioClip.GetContent(webRequest);
+                audioSource.clip = audioClip;
+                audioSource.Play();
+                Debug.Log("Audio is Playing");
+            }
+        }
+        
+            
+    }
+    //Remove Soon
+    public void ButtonClick()
+    {
+        url = "http://localhost/"+songDataReader.songTitle[0]+".mp3";
+        StartCoroutine(DownloadAudioClip());
+        Debug.Log("Starting to download the audio...");
+    }
+}
---
+++ Assets/Aset/Script/SongDataReader.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SongDataReader : MonoBehaviour
+{
+    public TextAsset textJSON;
+    public string[] songTitle;
+
+    [System.Serializable]
+    public class Song
+    {
+        public string title;
+    }
+
+    [System.Serializable]
+    public class SongList
+    {
+        public Song[] song;
+    }
+
+    public SongList songList = new SongList();
+
+
+    void Start()
+    {
+        songList = JsonUtility.FromJson<SongList>(textJSON.text);
+        int i;
+     
+        i = songList.song.Length;
+        songTitle = new string[i];
+        
+        for (i = 0; i < songList.song.Length; i++)
+        {
+            Song song = songList.song[i];
+            songTitle[i] = song.title;
+
+        }
+        
+    }
+}
+
+//Gak dipake
+//foreach (Song song in songList.song)
+//{
+//    Debug.Log(song.title);
+//}
